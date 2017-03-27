a6b54bbf17827d0c9ce8ea2e02ac1f1675493b42
jorge <jorge@qw>
2017-03-27 06:35:59 -0600

    Bussines rules management

---
+++ application/views/include/head.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>jQuery Bootstrap Submenu Plugin Demo</title>
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap-submenu.min.css">
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/docs.min.css">
+
+<script src="http://code.jquery.com/jquery-2.1.1.min.js" defer></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js" defer></script>
+<script src="<?php echo base_url(); ?>assets/js/bootstrap-submenu.min.js" defer></script>
+<script src="<?php echo base_url(); ?>assets/js/docs.js" defer></script>
+
+</head>
+<body>
+  
